3add179cf4aaa2ab66e6d8cbf2f38f7ef3a5a458
Anoy Roy Chowdhury <anoyroyc3545@gmail.com>
2024-02-04 00:24:46 +0530

    Added Contacts Tab

--- src/app/(dashboard)/chats/page.tsx
+++ src/app/(dashboard)/chats/page.tsx
@@ -1,5 +1,11 @@
+import ContactsTab from "@/components/layout/chats/contacts/ContactsTab";
+
 const ChatPage = () => {
-  return <div className="flex-1">ChatPage</div>;
+  return (
+    <div className="flex flex-1 flex-grow bg-white/80 rounded-l-xl">
+      <ContactsTab />
+    </div>
+  );
 };
 
 export default ChatPage;
